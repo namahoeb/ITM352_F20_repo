f74ca8d71d74c20874873f144a17fba443d267fe
namahoeb <70194447+namahoeb@users.noreply.github.com>
2020-09-23 13:20:54 -1000

    Lab6 and Invoice1 WOD

---
+++ Lab6/Ex1.js
@@ -0,0 +1,7 @@
+var age=19;
+var fav_num=2019;
+var day_of_birth=31;
+var month_of_birth=3;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+console.log((age + fav_num) / day_of_birth * month_of_birth);
---
+++ Lab6/Ex4.js
@@ -0,0 +1,8 @@
+n = 2;
+age = 19;
+
+console.log(n = n + age);
+console.log(n += age);
+
+z=10;
+console.log(n += z);
