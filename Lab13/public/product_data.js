08c2d1d3af77d7a8f9a6fc6fb0cfcfbeee624718
namahoeb <70194447+namahoeb@users.noreply.github.com>
2020-11-06 21:06:16 -1000

    Lab13

---
+++ Lab13/public/product_data.js
@@ -0,0 +1,16 @@
+products = 
+[
+  {  
+  "model":"Apple iPhone XS",  
+  "price": 990.00
+  },
+  {  
+  "model":"Samsung Galaxy",  
+  "price": 240.00  
+  }
+];
+
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
+  
