407c9f6dee9b0f3b390f9e15b0623ac68b3ab083
namahoeb <70194447+namahoeb@users.noreply.github.com>
2020-11-10 17:14:41 -1000

    Lab 14

---
+++ Lab14/Ex1.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+var user_data_filename = 'user_data.json';
+
+var data = fs.readFileSync(user_data_filename, 'utf-8');
+
+users_reg_data = JSON.parse(data);
+
+//console.log(users_reg_data, typeof users_reg_data, typeof data);
+
+console.log(users_reg_data['dport']['password']);
