2d5a3c2fa387ba628b9fb64fd89d67e4820c594d
namahoeb <namahoeb@hawaii.edu>
2020-11-16 11:26:49 -1000

    Lab 14 Edit
    Lab 14 Edit

--- Lab14/Ex2.js
+++ Lab14/Ex2.js
@@ -4,10 +4,9 @@ const user_data_filename = 'user_data.json';
 
 // check if file exists before reading
 if (fs.existsSync(user_data_filename) ) {
-    
-    stats = fs.statSync (user_data_filename);
-    console.log
 
+
+    
     var data = fs.readFileSync(user_data_filename, 'utf-8');
     users_reg_data = JSON.parse(data);
 }
@@ -16,7 +15,6 @@ if(typeof users_reg_data['itm352'] != 'undefined') {
     console.log(users_reg_data['itm352']['password']=='xxx');
 }
 
-else
 console.log(`ERR : ${user_data_filename} does not exist!!!`)
 
 //console.log(users_reg_data, typeof users_reg_data, typeof data);
