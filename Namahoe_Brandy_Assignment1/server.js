9bd9a6a97c212001cd87948da6add2dd4773f122
namahoeb <namahoeb@hawaii.edu>
2020-11-19 23:29:58 -1000

    Assignement 2 Setup
    Assignment 2 setup, Assignment 1 Edits

--- Namahoe_Brandy_Assignment1/server.js
+++ Namahoe_Brandy_Assignment1/server.js
@@ -6,25 +6,23 @@
 // To access code from node packages
 var express = require('express'); // To load express module
 var myParser = require("body-parser"); // To load and cache body parser module
-var products = require('./products.js'); // To load products.json file and set to variable 'products'
+var products = require('../products.js'); // To load products.json file and set to variable 'products'
 var fs = require('fs');
 var app = express(); // To load express 'app'
 const queryString = require('query-string'); // Read variable 'queryString' as the loaded query-string module
 
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path); // Write in the console the request method and its path
-    next();
+    next(); //continues
 });
 
 app.use(myParser.urlencoded({ extended: true })); // puts products in the body
 
 // Response when /process_form is requested or purchase form is submitted
-app.post("process_form", function (request, response, next) { // accesses invoice page
+app.post("/process_form", function (request, response) { // accesses invoice page
     let POST = request.body; // products POSTed in body
-    params = request.query;
-    console.log(params);
 
-    //check if quantities are nonnegative integers 
+    // Check if quantities are non-negative integers 
     if (typeof POST['submitPurchase'] != 'undefined') {
         var hasvalidquantities = true; // creating a variable assuming that it is true
         var hasquantities = false
@@ -37,15 +35,15 @@ app.post("process_form", function (request, response, next) { // accesses invoic
         // if all quantities are valid, will generate the invoice
         const stringified = queryString.stringify(POST);
         if (hasvalidquantities && hasquantities) {
-            response.redirect("./invoice.html?"+stringified); // using the invoice.html and all the data that is input
+            response.redirect("./invoice.html?"+stringified); // using the invoice.html + data from string
         }  
         else { 
-            response.redirect("./products_display.html?" + stringified) 
+            response.send('Enter a valid quantity!') 
         }
     }
 });
 
-//repeats the isNonNegInt function from the products_display.html file 
+//repeats isNonNegInt function from the products_display.html file 
 function isNonNegInt(q, returnErrors = false) {
     errors = []; // assume that quantity data is valid 
     if (q == "") { q = 0; }
@@ -55,5 +53,5 @@ function isNonNegInt(q, returnErrors = false) {
     return returnErrors ? errors : (errors.length == 0);
 }
 
-app.use(express.static('./public')); // root in the 'public' directory so that express will serve up files from here
+app.use(express.static('./public')); // root in the 'public' directory so that express will serve up files from public
 app.listen(8080, () => console.log(`listening on port 8080`)); //run the server on port 8080 and show it in the console
