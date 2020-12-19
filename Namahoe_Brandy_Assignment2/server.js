acf87a3795fa2a80459a4be9f0ba2ec407c1ad32
namahoeb <namahoeb@hawaii.edu>
2020-12-18 14:27:47 -1000

    Assignment 2 Edits

--- Namahoe_Brandy_Assignment2/server.js
+++ Namahoe_Brandy_Assignment2/server.js
@@ -7,7 +7,7 @@
 var products = require('./products.json'); // Sets products.json data into a variable called data
 const querystring = require('querystring'); // Sets querystring module as variable "querystring"
 var express = require('express'); // Begins express module
-var app = express (); // Sets express as variable "app"
+var app = express(); // Sets express as variable "app"
 var myParser = require("body-parser"); // Sets body-parser module as variable "myParser"
 var cookieParser = require('cookie-parser'); //sets require cookie parser as variable "cookieParser"
 var products_data = require('./products.json') //loads product data 
@@ -16,7 +16,7 @@ var fs = require('fs');
 // Processes incoming requests
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path); // generates request method and request path in console
-    next(); 
+    next();
 });
 
 app.use(myParser.urlencoded({ extended: true }));
@@ -26,32 +26,32 @@ app.get("/product_data.js", function (request, response, next) {
     str = `var products = ${JSON.stringify(products)}`;
     response.type('.js');
     response.send(str);
-    });
+});
 
 app.use(cookieParser());
-app.get("/login", function (request, response){ //responds with cookie
-    response.cookie('username', 'Natalie', {maxAge: 300*1000}).send('cookie set!'); //Sets login username, sends cookie and cookie expiration time
+app.get("/login", function (request, response) { //responds with cookie
+    response.cookie('username', 'Natalie', { maxAge: 300 * 1000 }).send('cookie set!'); //Sets login username, sends cookie and cookie expiration time
 });
-app.get("/logout", function (request, response){ //responds with cookie
+app.get("/logout", function (request, response) { //responds with cookie
     username = request.cookies.username //identifies username of who is being logged out
     response.clearCookie('username').send(`logged out ${username}`); //clears cookie upon logout
 });
 
-app.post("/process_form", function (request, response) { 
-     POST = request.body;
-    response.send(POST); 
+app.post("/process_form", function (request, response) {
+    POST = request.body;
+    response.send(POST);
 });
 
 // processing purchase
 app.post("/process_purchase", function (request, response) {
     //process_purchase(request.body, response);
-     POST = request.body;
-     if (typeof POST['submitPurchase'] != 'undefined') {
-        
+    POST = request.body;
+    if (typeof POST['submitPurchase'] != 'undefined') {
+
         let hasqty = false;
         let hasErrors = false; // assume no errors to begin with
         //validate each quantity 
-        for (i in products) { 
+        for (i in products) {
             qty = POST[`quantity${i}`];
             //check if quantity is non negative integer
             if (qty != '' && isNonNegInteger(qty) == false) {
@@ -75,10 +75,10 @@ app.post("/process_purchase", function (request, response) {
         else { //if no quantity inputted or invalid, notify user and send them back to display
             alertstr = `<script> alert("Error! You need to select a item or enter valid quantities");
                         window.history.back() </script>`;
-            
+
             response.send(alertstr);
-         }
-    } 
+        }
+    }
 });
 
 const user_data_filename = 'user_data.json'; // user_data.json assigned to user_data_filename
@@ -86,28 +86,28 @@ var data = fs.readFileSync(user_data_filename, 'utf-8'); // reads data in user_d
 users_data = JSON.parse(data); // parses data variable into JSON them passes it to users_reg_data
 
 //This takes the login info from login_form on user_login.html, checks if user exists in userdata.json, if they do and password is correct, then redirect to invoice. 
-app.post("/process_login", function (request, response) { 
+app.post("/process_login", function (request, response) {
 
     var POST = request.body
     console.log(quantity_data);
     if (typeof users_data[request.body.username] != 'undefined' && typeof quantity_data != 'undefined') { //if username exists in userdata.json retrieve their data
-        if (request.body.password != users_data[request.body.username].password) { 
+        if (request.body.password != users_data[request.body.username].password) {
             var contents = fs.readFileSync('invoice.html'); response.send(eval('`' + contents + '`')); //renders template string
         }
 
-            alertstr = `<script> alert("Password is incorrect!");
+        alertstr = `<script> alert("Password is incorrect!");
             window.history.back() </script>`;
 
-            response.send(alertstr); // send alert
+        response.send(alertstr); // send alert
 
 
-        } else { // if the password does match 
-            string_with_quantities = request.query; //get quantities string
-            string_with_username = request.body; // get username string
-            console.log(request);
+    } else { // if the password does match 
+        string_with_quantities = request.query; //get quantities string
+        string_with_username = request.body; // get username string
+        console.log(request);
 
-            response.redirect("./invoice.html?" + querystring.stringify(string_with_quantities) + "&" + querystring.stringify(string_with_username)); // redirect to invoice with the two strings
-        }
+        response.redirect("./invoice.html?" + querystring.stringify(string_with_quantities) + "&" + querystring.stringify(string_with_username)); // redirect to invoice with the two strings
+    }
 });
 
 //This processes a user's registration info  and checks if it is valid, if data is valid it redirects user from registration to invoice
@@ -136,7 +136,7 @@ app.post("/process_register", function (request, response) {
 
     if (/^[A-Za-z]+$/.test(request.body.name)) { // if full name contains characters beside letters code from Kylee Dean-Kobatake and W3schools
     } else {
-        errors.push(("Username can only contain letters or numbers.")); 
+        errors.push(("Username can only contain letters or numbers."));
         //sends error if username has characters other than letters 
     }
 
@@ -180,12 +180,12 @@ app.post("/process_register", function (request, response) {
 // isNonNegInteger function from Lab 12 which checks if quantities entered are a non-negative integer
 function isNonNegInteger(q, returnErrors = false) {
     errors = []; //assume no errors at first
-/* This function returns true if value of q is a non-negative integer
-*/
-    if(Number(q) != q) errors.push('Not a number!');
+    /* This function returns true if value of q is a non-negative integer
+    */
+    if (Number(q) != q) errors.push('Not a number!');
     // Checks if string is a number
-    if(q < 0) errors.push('Negative value!'); // Checks if string is non-negative
-    if(parseInt(q) != q) errors.push('Not an integer!'); // Checks if string is an integer
+    if (q < 0) errors.push('Negative value!'); // Checks if string is non-negative
+    if (parseInt(q) != q) errors.push('Not an integer!'); // Checks if string is an integer
     return returnErrors ? errors : (errors.length == 0);
 }
 app.use(express.static('./public')); // Searches for files in "public"
